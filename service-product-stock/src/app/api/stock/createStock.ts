16931f88a3a1bc37b88908e38899730aa25bf5d3
iluha168 <illya.chernyshov888@gmail.com>
2024-11-19 22:41:31 +0700

    /api/stock endpoint created

---
+++ service-product-stock/src/app/api/stock/createStock.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server"
+import { z } from "zod"
+
+import { createRouteJSON } from "@/app/templates/createRouteJSON"
+import { db } from "@/../../shared/db"
+
+const zInt = z.number().int()
+
+export const { POST, fetch: createStock } = createRouteJSON(
+	"POST",
+	"/api/stock",
+	z.object({
+		shop_id: zInt,
+		product_plu: zInt,
+		amount_shelf: zInt,
+		amount_ordered: zInt,
+	}),
+	async (body) => {
+		const [stock] = await db.$transaction([
+			db.stock.create({
+				data: body,
+			}),
+
+			db.log.create({
+				data: {
+					shop_id: body.shop_id,
+					product_plu: body.product_plu,
+					action: "STOCK_CREATED",
+				},
+			}),
+		])
+
+		return NextResponse.json(stock)
+	},
+)
---
+++ service-product-stock/src/app/api/stock/filterStock.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server"
+import { z } from "zod"
+
+import { db } from "@/../../shared/db"
+import { createRouteSearchParams } from "@/app/templates/createRouteSearchParams"
+
+const zStrIntOpt = z.coerce.number().int().optional()
+
+export const { GET, fetch: filterStock } = createRouteSearchParams(
+	"GET",
+	"/api/stock",
+	z.object({
+		plu: zStrIntOpt,
+		shop_id: zStrIntOpt,
+
+		shelf_min: zStrIntOpt,
+		shelf_max: zStrIntOpt,
+
+		ordered_min: zStrIntOpt,
+		ordered_max: zStrIntOpt,
+	}),
+	async (body) => {
+		const stocks = await db.stock.findMany({
+			where: {
+				product_plu: body.plu,
+				shop_id: body.shop_id,
+				amount_shelf: {
+					gte: body.shelf_min,
+					lte: body.shelf_max,
+				},
+				amount_ordered: {
+					gte: body.ordered_min,
+					lte: body.ordered_max,
+				},
+			},
+		})
+		return NextResponse.json(stocks)
+	},
+)
