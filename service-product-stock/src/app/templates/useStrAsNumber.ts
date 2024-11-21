8a6e410f6179152cff46e061c880dc50a355062a
IlyaBoykoAtWork <ilyab128@gmail.com>
2024-11-21 14:50:24 +0700

    Add option for default value in useStrAsNumber

--- service-product-stock/src/app/templates/useStrAsNumber.ts
+++ service-product-stock/src/app/templates/useStrAsNumber.ts
@@ -9,7 +9,7 @@ const parserInputNumber = z.string().min(1).pipe(z.coerce.number())
  * Creates string state with 2 getters:
  * coerced number and the string itself
  */
-export default function useStrAsNumber() {
-	const [str, set] = useState<string>("")
+export default function useStrAsNumber(init = "") {
+	const [str, set] = useState<string>(init)
 	return [parserInputNumber.safeParse(str).data, str, set] as const
 }
