c7fd16af0cb8b02cd0849a6f4223e710e29d977d
IlyaBoykoAtWork <ilyab128@gmail.com>
2024-11-21 15:50:09 +0700

    Home page now links to other pages

--- service-product-stock/src/app/page.tsx
+++ service-product-stock/src/app/page.tsx
@@ -1,3 +1,26 @@
+import clsx from "clsx"
+import Link from "next/link"
+
 export default function Home() {
-	return null
+	return (
+		<nav className="flex flex-wrap">
+			{["/products", "/stocks"].map((href) => (
+				<div
+					key={href}
+					className={clsx(
+						"p-8 m-4",
+						"bg-slate-400 dark:bg-slate-700",
+						"filter hover:contrast-125",
+						"relative top-0 hover:top-2",
+						"transition-[filter,top]",
+						"rounded-lg shadow-lg",
+					)}
+				>
+					<Link href={href} className="text-6xl underline">
+						{href}
+					</Link>
+				</div>
+			))}
+		</nav>
+	)
 }
