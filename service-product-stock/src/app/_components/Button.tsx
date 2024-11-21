312961b725872998299b6e2996d4981f5d925a9a
IlyaBoykoAtWork <ilyab128@gmail.com>
2024-11-21 13:56:20 +0700

    Divert attention from disabled Buttons

--- service-product-stock/src/app/_components/Button.tsx
+++ service-product-stock/src/app/_components/Button.tsx
@@ -8,10 +8,9 @@ export default function Button(props: JSX.IntrinsicElements["button"]) {
 				"p-1",
 				"bg-gradient-to-br from-zinc-500 via-zinc-300 to-zinc-500",
 				"dark:from-zinc-800 dark:via-zinc-700 dark:to-zinc-800",
-				props.disabled || [
+				props.disabled ? "blur-[2px]" : [
 					"hover:from-zinc-400 hover:via-zinc-200 hover:to-zinc-400",
 					"hover:dark:from-zinc-700 hover:dark:via-zinc-600 hover:dark:to-zinc-700",
-					"opacity-90",
 				],
 				"rounded-md shadow-inner",
 				props.className,
