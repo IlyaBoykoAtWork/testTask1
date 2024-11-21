4c16995599c5f022ee944d3742cd17a9420f1925
IlyaBoykoAtWork <ilyab128@gmail.com>
2024-11-21 13:18:32 +0700

    Removed min Input width

--- service-product-stock/src/app/_components/Input.tsx
+++ service-product-stock/src/app/_components/Input.tsx
@@ -12,7 +12,7 @@ export default function Input(props: InputProps) {
 			{...props}
 			onChange={(e) => props.onChange(e.target.value)}
 			className={clsx(
-				"p-1",
+				"p-1 min-w-0",
 				"bg-blue-500 dark:bg-blue-900",
 				"border border-solid border-slate-900 dark:border-slate-200",
 				"rounded-md shadow-inner",
